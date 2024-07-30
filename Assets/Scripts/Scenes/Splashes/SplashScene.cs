eeef87fc423551855af596fcd139809131110cd3
dyguests <dyguests@qq.com>
2024-07-31 00:35:11 +0800

    SplashState

---
+++ Assets/Scripts/Scenes/Splashes/SplashScene.cs
@@ -0,0 +1,13 @@
+using Koyou.Frameworks;
+
+namespace Scenes.Splashes
+{
+    public class SplashScene : BaseScene
+    {
+        // Start is called before the first frame update
+        void Start() { }
+
+        // Update is called once per frame
+        void Update() { }
+    }
+}
---
+++ Assets/Scripts/Scenes/Splashes/SplashState.cs
@@ -0,0 +1,26 @@
+﻿using Cysharp.Threading.Tasks;
+using Koyou.Frameworks;
+
+namespace Scenes.Splashes
+{
+    public class SplashState : AppState
+    {
+        #region AppState
+
+        public override async UniTask Enter()
+        {
+            await base.Enter();
+            
+            await UniTask.Delay(1000);
+            
+            
+        }
+
+        public override async UniTask Exit()
+        {
+            await base.Exit();
+        }
+
+        #endregion
+    }
+}
