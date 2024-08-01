4076d21e1f3feade4c6a495e4db8cbedb65fb883
fanhl <dyguests@qq.com>
2024-08-02 01:08:50 +0800

    lobby

---
+++ Assets/Scripts/Scenes/Lobbies/LobbyAppState.cs
@@ -0,0 +1,24 @@
+﻿using Cysharp.Threading.Tasks;
+using Koyou.Frameworks;
+
+namespace Scenes.Lobbies
+{
+    public class LobbyAppState : AppState
+    {
+        #region AppState
+
+        public override async UniTask Enter()
+        {
+            await base.Enter();
+
+            // todo
+        }
+
+        public override async UniTask Exit()
+        {
+            await base.Exit();
+        }
+
+        #endregion
+    }
+}
