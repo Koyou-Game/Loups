eb410e0f018a489b75af7157ebca7002de26d90e
fanhl <dyguests@qq.com>
2024-08-02 22:16:13 +0800

    LobbyView

---
+++ Assets/Scripts/Scenes/Lobbies/LobbyView.cs
@@ -0,0 +1,23 @@
+﻿using Cysharp.Threading.Tasks;
+using Entities;
+using Koyou.Frameworks;
+
+namespace Scenes.Lobbies
+{
+    public class LobbyView : DataView<Lobby>
+    {
+        #region DataView<Lobby>
+
+        public override async UniTask LoadData(Lobby data)
+        {
+            await base.LoadData(data);
+        }
+
+        public override async UniTask UnloadData()
+        {
+            await base.UnloadData();
+        }
+
+        #endregion
+    }
+}
