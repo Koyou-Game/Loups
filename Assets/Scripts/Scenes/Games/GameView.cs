dfe99f32eb7396b1b4bd485ac9414565ee53fb16
dyguests <dyguests@qq.com>
2024-08-03 10:41:25 +0800

    level controller

--- Assets/Scripts/Scenes/Games/GameView.cs
+++ Assets/Scripts/Scenes/Games/GameView.cs
@@ -1,7 +1,32 @@
-﻿using Entities;
+﻿using Cysharp.Threading.Tasks;
+using Entities;
 using Koyou.Frameworks;
+using Koyou.Kogine.Side.Cores;
+using UnityEngine;
 
 namespace Scenes.Games
 {
-    public class GameView : DataView<Game> { }
+    public class GameView : DataView<Game>
+    {
+        #region DataView<Game>
+
+        public override async UniTask LoadData(Game data)
+        {
+            await base.LoadData(data);
+            await levelController.Load();
+        }
+
+        public override async UniTask UnloadData()
+        {
+            await base.UnloadData();
+        }
+
+        #endregion
+
+        #region GameView
+
+        [SerializeField] private LevelController levelController;
+
+        #endregion
+    }
 }
