0c58ed331f88138effb3a4091a3bdb5caa24628f
fanhl <dyguests@qq.com>
2024-08-22 23:37:27 +0800

    AssetBundles

---
+++ Assets/Editor/AssetBundleBuilder.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+
+public static class AssetBundleBuilder
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        BuildPipeline.BuildAssetBundles("Assets/AssetBundles", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
+}
