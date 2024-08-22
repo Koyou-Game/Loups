eed3e44d933a7ddde285590c1665af53b3b9bf39
fanhl <dyguests@qq.com>
2024-08-22 23:40:54 +0800

    Assets/BuildAssetBundles

--- Assets/Editor/AssetBundleBuilder.cs
+++ Assets/Editor/AssetBundleBuilder.cs
@@ -1,10 +1,25 @@
-﻿using UnityEditor;
+﻿using System.IO;
+using Koyou.Commons;
+using UnityEditor;
 
 public static class AssetBundleBuilder
 {
     [MenuItem("Assets/Build AssetBundles")]
     static void BuildAllAssetBundles()
     {
-        BuildPipeline.BuildAssetBundles("Assets/AssetBundles", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+        // 设置目标路径
+        string assetBundleDirectory = "Assets/BuildAssetBundles";
+
+        // 检查目录是否存在，如果不存在则创建
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+            Log.N($"Created directory: " + assetBundleDirectory);
+        }
+
+        // 构建 AssetBundles
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+
+        Log.N($"AssetBundles built successfully!");
     }
 }
