6f45e2c594fd7ca88efe8d0e44295382c9248ccc
dyguests <dyguests@qq.com>
2024-08-23 20:52:50 +0800

    level save/load

--- Assets/Scripts/Scenes/Workshops/WorkshopUiView.cs
+++ Assets/Scripts/Scenes/Workshops/WorkshopUiView.cs
@@ -1,6 +1,25 @@
-﻿using UnityEngine;
+﻿using Koyou.Commons;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Scenes.Workshops
 {
-    public class WorkshopUiView : MonoBehaviour { }
+    public class WorkshopUiView : MonoBehaviour
+    {
+        #region WorkshopUiView
+
+        [SerializeField] private Button saveBtn;
+
+        private void Start()
+        {
+            saveBtn.onClick.AddListener(OnSaveBtnClick);
+        }
+
+        private void OnSaveBtnClick()
+        {
+            Log.N($"OnSaveBtnClick");
+        }
+
+        #endregion
+    }
 }
