b0009f8f4b45935a74d80ced6873dab13766ccb8
dyguests <dyguests@qq.com>
2024-08-23 21:54:35 +0800

    ShapeSketcher

--- Assets/Scripts/Sketches/ShapeSketcher.cs
+++ Assets/Scripts/Sketches/ShapeSketcher.cs
@@ -1,11 +1,16 @@
 ﻿using Sketches.Entities;
+using UnityEngine;
 
 namespace Sketches
 {
     public class ShapeSketcher : PlacementSketcher
     {
+        [SerializeField] private SpriteRenderer sr;
+        [SerializeField] private new Collider2D collider;
+
         public override ISketch ToSketch()
         {
+            // todo
             return new ShapeSketch();
         }
     }
