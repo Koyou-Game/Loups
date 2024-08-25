f0d90973ad954d3464c3b6ecc1204d39b38936d2
fanhl <dyguests@qq.com>
2024-08-25 20:32:14 +0800

    save works

--- Assets/Scripts/Storages/LevelRepository.cs
+++ Assets/Scripts/Storages/LevelRepository.cs
@@ -1,9 +1,28 @@
-﻿using Sketches.Entities;
+﻿using System.IO;
+using Koyou.Commons;
+using Sketches.Entities;
+using UnityEngine;
 
 namespace Storages
 {
-    public class LevelRepository
+    public static class LevelRepository
     {
-        public static void Save(ILevelSketch sketch, string story, string level) { }
+        public static void Save(ILevelSketch sketch, string story, string level)
+        {
+            // 构建文件路径
+            var directoryPath = Path.Combine(Application.persistentDataPath, story);
+            var levelPath = Path.Combine(directoryPath, $"{level}.json");
+            Log.N($"directoryPath:\t{directoryPath}");
+            Log.N($"levelPath:\t{levelPath}");
+
+            // 如果文件夹不存在，则创建文件夹
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            var json = JsonUtil.SerializeObject(sketch);
+            File.WriteAllText(levelPath, json);
+        }
     }
 }
