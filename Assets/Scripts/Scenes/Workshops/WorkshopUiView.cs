20cce2c058873f183d93d40e29a48a5488db5897
fanhl <dyguests@qq.com>
2024-09-15 17:43:08 +0800

    PointListener

--- Assets/Scripts/Scenes/Workshops/WorkshopUiView.cs
+++ Assets/Scripts/Scenes/Workshops/WorkshopUiView.cs
@@ -1,12 +1,27 @@
-﻿using UnityEngine;
+﻿using Koyou.Commons;
+using Koyou.Indicators;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Scenes.Workshops
 {
     public class WorkshopUiView : MonoBehaviour
     {
+        #region MonoBehaviour
+
+        private void Awake()
+        {
+            indicatorManager.pointListener = new PointListener(this);
+        }
+
+        #endregion
+
         #region WorkshopUiView
 
+        [SerializeField]
+        private IndicatorManager indicatorManager;
+
+        [Space]
         [SerializeField] private Button playBtn;
         [SerializeField] private Button saveBtn;
 
@@ -33,6 +48,35 @@ namespace Scenes.Workshops
         {
             void OnPlayClick();
             void OnSaveClick();
+        }
+
+        private class PointListener : IndicatorManager.IPointListener
+        {
+            private WorkshopUiView _owner;
+
+            public PointListener(WorkshopUiView owner)
+            {
+                _owner = owner;
+            }
+
+            #region IndicatorManager.IPointListener
+
+            public void OnPointerDown(Vector2Int pos, Vector3 positionSnap)
+            {
+                Log.N($"Called");
+            }
+
+            public void OnPointerMove(Vector2Int pos, Vector3 positionSnap)
+            {
+                Log.N($"Called");
+            }
+
+            public void OnPointerUp(Vector2Int pos, Vector3 positionSnap)
+            {
+                Log.N($"Called");
+            }
+
+            #endregion
         }
 
         #endregion
