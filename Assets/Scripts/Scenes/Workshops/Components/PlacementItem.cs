a376d606664c15e29b0c0ca2d40a49fff33d2294
fanhl <dyguests@qq.com>
2024-09-15 17:13:57 +0800

    placementItem

---
+++ Assets/Scripts/Scenes/Workshops/Components/PlacementItem.cs
@@ -0,0 +1,13 @@
+﻿using Sketches;
+using UnityEngine;
+
+namespace Scenes.Workshops.Components
+{
+    /// <summary>
+    /// Workshop场景中插入Placement的共通按钮
+    /// </summary>
+    public class PlacementItem : MonoBehaviour
+    {
+        [SerializeField] private Sketcher sketcher;
+    }
+}
